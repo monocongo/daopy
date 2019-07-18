b2b73244d43ba4e70c3181f27b85426bfba84d50
James Adams <monocongo@gmail.com>
2019-07-18 16:31:32 -0400

    stub test functions
    #2

---
+++ tests/test_dao.py
@@ -0,0 +1,28 @@
+import pytest
+
+from daopy import dao
+
+
+def test_delete():
+
+    pass
+
+
+def test_find():
+
+    pass
+
+
+def test_insert():
+
+    pass
+
+
+def test_insert_all():
+
+    pass
+
+
+def test_update():
+
+    pass
