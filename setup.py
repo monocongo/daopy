07ae7849b4c789bedb980784692697f3c364f4d5
James Adams <monocongo@gmail.com>
2019-07-18 20:07:24 -0400

    changed to MIT license before upload to PyPI
    #7

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
-        "License:: OSI Approved:: GNU General Public License v3 or later(GPLv3 +)",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
